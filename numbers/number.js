4d79c257d59e5172da174c0fc7a1d52a043e3aac
Jamie <lydiaxi.wow@gmail.com>
2021-03-16 06:50:01 -0700

    init project

---
+++ numbers/number.js
@@ -0,0 +1,46 @@
+const numberLib = () => {
+    function add(a, b) {
+    }
+
+    function minus(a, b) {
+    }
+
+    function multiply(a, b) {
+    }
+
+    function devide(a, b) {
+    }
+
+    function exponentiation(a, b) {
+    }
+
+    function incroment(a, b) {
+    }
+
+    function decrement(a, b) {
+    }
+
+    function numberList(start, end) {
+    }
+
+    function numberListWithSkip(start, count, skipEvery) {
+    }
+
+    function createRandomNumber(min, max) {
+    }
+
+    function randomizeNumberList(start, count, isRandom) {
+    }
+
+    function isOdd(num) {
+    }
+
+    function isEven(num) {
+    }
+
+    function randomNumberList(start, end) {
+    }
+
+    function randomizeNumberListB(start, end, count, allowDuplicates) {
+    }
+}
