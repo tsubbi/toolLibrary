c7a06af306043d096842f8e382b6a551268e1227
Jamie <jamie.yjchen@gmail.com>
2021-03-16 21:20:19 -0700

    finish string lib

--- string/string.js
+++ string/string.js
@@ -2,9 +2,7 @@ import { utilities } from '../utilties.mjs';
 
 const stringLib = () => {
     function concatanate(a, b) {
-        let result = a;
-        result.concatanate(b);
-        return result;
+        return a+b;
     }
 
     function ASCIINumberToChar(num) {
@@ -20,5 +18,49 @@ const stringLib = () => {
 
     function reverseString(str) {
         return (str === '') ? '' : reverseString(str.substr(1)) + str.charAt(0);
+    }
+
+    function generateOnlyUpperCase() {
+        let aList = [];
+        // ascii code from 65-90 are upper cases
+        for (i=65; i<=90; i++) {
+            aList.push(ASCIINumberToChar(i));
+        }
+
+        return aList;
+    }
+
+    function generateOnlyLowerCase() {
+        let aList = [];
+        // ascii code from 97-122 are lower cases
+        for (i=97; i<=122; i++) {
+            aList.push(ASCIINumberToChar(i));
+        }
+
+        return aList;
+    }
+
+    function generateOnlyNumbers() {
+        let aList = [];
+        // ascii code from 48-57 are numbers
+        for (i=48; i<=57; i++) {
+            aList.push(ASCIINumberToChar(i));
+        }
+
+        return aList;
+    }
+
+    function generateOnlySymbols() {
+        let aList = [];
+        for (i=33; i<=126; i++) {
+            if ((i >= 48 && i <= 57) || (i >= 65 && i<= 90) || (i>=97 && i<=122)) { continue }
+            aList.push(ASCIINumberToChar(i));
+        }
+
+        return aList;
+    }
+
+    function generateUpperAndLowerCase() {
+        return generateOnlyUpperCase().concat(generateOnlyLowerCase());
     }
 }
